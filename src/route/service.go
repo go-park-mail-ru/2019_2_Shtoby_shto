cad6c0ae7a3088b23737ca3a9d2e400130703838
Aleksandr <khoroshenin@saject.ru>
2019-09-29 12:05:23 +0300

    T8 change logout method

--- src/route/service.go
+++ src/route/service.go
@@ -48,7 +48,7 @@ func NewRouterService(s security.Security) *mux.Router {
 	r.HandleFunc("/docs/", httpSwagger.WrapHandler)
 	r.HandleFunc("/", nil)
 	r.HandleFunc("/login", s.Login).Methods(http.MethodPost, http.MethodOptions)
-	r.HandleFunc("/logout", s.Logout).Methods(http.MethodGet, http.MethodOptions)
+	r.HandleFunc("/logout", s.Logout).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/registration", s.Registration).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/user/{id}", s.CheckSession(s.UpdateUserSecurity)).Methods(http.MethodPut, http.MethodOptions)
 	return r
