a29d8584919427cf1c08af3fee5e5b7432ae8f4a
Aleksandr <khoroshenin@saject.ru>
2019-09-29 11:23:54 +0300

    T8 add header credentials

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -64,6 +64,7 @@ func Join(args ...string) string {
 func SetHeaders(w *http.ResponseWriter) {
 	(*w).Header().Set("Content-Type", "application/json")
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers",
 		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
