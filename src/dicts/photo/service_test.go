5e927fa42131e9c63312bbfd9dcc21ecb1b51988
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2019-11-27 18:50:06 +0300

    phototest

--- src/dicts/photo/service_test.go
+++ src/dicts/photo/service_test.go
@@ -1,16 +1,30 @@
 package photo
 
 import (
+	"2019_2_Shtoby_shto/file_service/file"
+	"2019_2_Shtoby_shto/src/config"
 	"2019_2_Shtoby_shto/src/customType"
 	"2019_2_Shtoby_shto/src/database"
+	"2019_2_Shtoby_shto/src/dicts"
+	"2019_2_Shtoby_shto/src/dicts/models"
+	"2019_2_Shtoby_shto/src/handle"
+	"2019_2_Shtoby_shto/src/utils"
 	"bufio"
+	"bytes"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCreateInstance(t *testing.T) {
 	type args struct {
-		db database.IDataManager
+		db         database.IDataManager
+		cfg        *config.Config
+		fileLoader file.IFileLoaderManagerClient
 	}
 	tests := []struct {
 		name string
@@ -21,7 +35,7 @@ func TestCreateInstance(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CreateInstance(tt.args.db); !reflect.DeepEqual(got, tt.want) {
+			if got := CreateInstance(tt.args.db, tt.args.cfg, tt.args.fileLoader); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("CreateInstance() = %v, want %v", got, tt.want)
 			}
 		})
@@ -29,39 +43,93 @@ func TestCreateInstance(t *testing.T) {
 }
 
 func Test_service_DownloadPhoto(t *testing.T) {
-	type args struct {
-		photo *bufio.Reader
+	db, _, err := sqlmock.New()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatalf("can't create mock: %s", err)
 	}
-	tests := []struct {
-		name    string
-		s       service
-		args    args
-		want    customType.StringUUID
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+
+	downloadPhotoServiceMock := NewMockHandlerPhotoService(gomock.NewController(t))
+	newdatabase := database.NewMockIDataManager(gomock.NewController(t))
+	newIFileLoader := file.NewIFileLoaderManagerClient(&grpc.ClientConn{})
+
+	downloadPhotoService := CreateInstance(newdatabase, &config.Config{}, newIFileLoader)
+
+	//gDB, err := gorm.Open("postgres", db)
+	//assert.Nil(t, err, "db error")
+
+	uuid, err := utils.GenerateUUID()
+	assert.Nil(t, err, "generate uuid error")
+
+	downloadPhotoServiceMock.EXPECT().DownloadPhoto(uuid.String()).Return(nil, nil)
+	newPhoto := &models.Photo{
+		BaseInfo: dicts.BaseInfo{
+			ID: customType.StringUUID(uuid.String()),
+		},
+		TimeLoad: time.Time{},
+		Path:     "/",
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.s.DownloadPhoto(tt.args.photo)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("service.DownloadPhoto() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("service.DownloadPhoto() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	data, _ := newPhoto.MarshalJSON()
+
+	downloadPhotoServiceMock.EXPECT().DownloadPhoto(data)
+	buf := bytes.NewReader(data)
+	r := bufio.NewReader(buf)
+	_, err = downloadPhotoService.DownloadPhoto(r)
+	assert.EqualError(t, err, "mkdir : The system cannot find the path specified.")
 }
 
 func Test_service_GetPhotoByUser(t *testing.T) {
+	db, _, err := sqlmock.New()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatalf("can't create mock: %s", err)
+	}
+
+	GetPhotoByUserMock := NewMockHandlerPhotoService(gomock.NewController(t))
+	newdatabase := database.NewMockIDataManager(gomock.NewController(t))
+	newIFileLoader := file.NewIFileLoaderManagerClient(&grpc.ClientConn{})
+
+	GetPhotoByUser := CreateInstance(newdatabase, &config.Config{}, newIFileLoader)
+
+	//gDB, err := gorm.Open("postgres", db)
+	//assert.Nil(t, err, "db error")
+
+	uuid, err := utils.GenerateUUID()
+	assert.Nil(t, err, "generate uuid error")
+
+	GetPhotoByUserMock.EXPECT().GetPhotoByUser(uuid.String()).Return(nil, nil)
+
+	newPhoto := &models.Photo{
+		BaseInfo: dicts.BaseInfo{
+			ID: customType.StringUUID(uuid.String()),
+		},
+		TimeLoad: time.Time{},
+		Path:     "/",
+	}
+
+	data, _ := newPhoto.MarshalJSON()
+
+	GetPhotoByUserMock.EXPECT().DownloadPhoto(data)
+	buf := bytes.NewReader(data)
+	r := bufio.NewReader(buf)
+	_, err = downloadPhotoService.DownloadPhoto(r)
+	assert.EqualError(t, err, "mkdir : The system cannot find the path specified.")
+
+	type fields struct {
+		HandlerImpl handle.HandlerImpl
+		cfg         *config.Config
+		db          database.IDataManager
+		fl          file.IFileLoaderManagerClient
+	}
 	type args struct {
 		photoID customType.StringUUID
 	}
 	tests := []struct {
 		name    string
-		s       service
+		fields  fields
 		args    args
 		want    []byte
 		wantErr bool
@@ -70,13 +138,19 @@ func Test_service_GetPhotoByUser(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.s.GetPhotoByUser(tt.args.photoID)
+			s := service{
+				HandlerImpl: tt.fields.HandlerImpl,
+				cfg:         tt.fields.cfg,
+				db:          tt.fields.db,
+				fl:          tt.fields.fl,
+			}
+			got, err := s.GetPhotoByUser(tt.args.photoID)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("service.GetPhotoByUser() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetPhotoByUser() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("service.GetPhotoByUser() = %v, want %v", got, tt.want)
+				t.Errorf("GetPhotoByUser() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
